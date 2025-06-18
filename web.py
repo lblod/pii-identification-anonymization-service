eb5c1407c9ac0d268a0f5f835b7f0941159dc8b5
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-06-18 12:25:43 +0200

    Add basic endpoint

--- web.py
+++ web.py
@@ -1,3 +1,28 @@
-@app.route("/hello")
-def hello():
-    return "Hello from the mu-python-template!"
+from flask import request, jsonify
+from presidio_analyzer import AnalyzerEngine
+
+analyzer = AnalyzerEngine()
+
+@app.route("/detect", methods=["POST"])
+def detect_pii():
+    """
+    Expect JSON: { "text": "<your text here>" }
+    Returns: list of PII spans with start/end/entity/score/match
+    """
+    
+    data = request.get_json(force=True, silent=True) or {}
+    text = data.get("text", "")
+    
+    results = analyzer.analyze(text=text, language="en")
+
+    output = []
+    for res in results:
+        output.append({
+            "start": res.start,
+            "end": res.end,
+            "entity": res.entity_type,
+            "score": round(res.score, 3),
+            "match": text[res.start:res.end]
+        })
+
+    return jsonify(output), 200
