f79a55dd9399a12819090535292135eb05c1748b
Martijn Bogaert <martijn.bogaert@sirus.be>
2025-06-24 10:01:08 +0200

    Use Dutch model

--- web.py
+++ web.py
@@ -1,11 +1,30 @@
 from flask import request, jsonify
 from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer.nlp_engine import NlpEngineProvider
 
-analyzer = AnalyzerEngine()
+nlp_configuration = {
+    "nlp_engine_name": "spacy",
+    "models": [
+        {"lang_code": "nl", "model_name": "nl_core_news_md"},
+        {"lang_code": "en", "model_name": "en_core_web_md"},
+    ],
+}
+
+provider = NlpEngineProvider(nlp_configuration=nlp_configuration)
+nlp_engine = provider.create_engine()
+
+analyzer = AnalyzerEngine(
+    nlp_engine=nlp_engine,
+    supported_languages=["nl", "en"],
+)
 
 
 def detect_pii(text: str):
-    results = analyzer.analyze(text=text, language="en")
+    try:
+        results = analyzer.analyze(text=text, language="nl")
+    except ValueError:
+        results = analyzer.analyze(text=text, language="en")
+
     return [
         {
             "start": r.start,
