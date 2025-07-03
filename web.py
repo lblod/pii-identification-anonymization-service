87661c63184847180d8dd7376c8431318d9eb5ee
Andreo Van Onckelen <andreo141mlg@gmail.com>
2025-07-03 13:10:14 +0200

    Add element id

--- web.py
+++ web.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import request, jsonify
 import xml.etree.ElementTree as ET
 from presidio import detect_pii
 
-def extract_text_from_bpmn(bpmn_content) -> list:
+def extract_elem_from_bpmn(bpmn_content) -> list:
+    elements = []
+    root = ET.fromstring(bpmn_content)
     try:
-        root = ET.fromstring(bpmn_content)
-        unique_texts = set()
-        
         for elem in root.iter():
             if 'name' in elem.attrib:
-                name_value = elem.attrib['name'].strip()
-                unique_texts.add(name_value)
-        
-        return list(unique_texts)
+                elements.append({
+                    "id": elem.attrib.get("id", ""),
+                    "name": elem.attrib['name'].strip(),
+                })
+        return list(elements)
     
     except ET.ParseError as e:
         raise ValueError(f"Invalid BPMN XML format: {str(e)}")
@@ -30,12 +30,16 @@ def process_bpmn_file(file):
     if isinstance(file_content, bytes):
         file_content = file_content.decode('utf-8')
     
-    extracted_texts = extract_text_from_bpmn(file_content)
-    combined_text = " ".join(extracted_texts)
-    pii_results = detect_pii(combined_text)
+    extracted_elements = extract_elem_from_bpmn(file_content)
+    pii_results = []
+    for elem in extracted_elements:
+        pii_result = detect_pii(elem['name'])
+        for key in pii_result:
+            key['id'] = elem['id']
+            pii_results.append(key)
     
     return {
-        "extracted_texts": extracted_texts,
+        "extracted_elements": extracted_elements,
         "pii_results": pii_results,
         "total_pii_found": len(pii_results)
     }
